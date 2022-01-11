c08fdef9dbee2b1472e253740648c6cef93c3c40
finma <firmanmaulana2403@gmail.com>
2022-01-11 11:48:43 +0700

    fix session

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import methodOverride from "method-override";
 import flash from "connect-flash";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import { MONGO_URL, SECRET_SESSION } from "./config/index.js";
+import { MONGO_URL } from "./config/index.js";
 
 //? WEB
 import userRouter from "./app/user/router.js";
@@ -40,7 +40,7 @@ app.set("view engine", "ejs");
 app.use(
   session({
     store: MongoStore.create({ mongoUrl: MONGO_URL }),
-    secret: SECRET_SESSION,
+    secret: "secret",
     resave: false,
     saveUninitialized: true,
     cookie: {},
